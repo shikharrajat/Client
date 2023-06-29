e03e34b5ab674ff1a55be7699b8191560975cf58
Shikhar <shikharsharma1408@gamil.com>
2023-06-29 17:24:02 +0530

    share image with community feature added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -31,7 +31,7 @@ const CreatePost = () => {
         })
 
         const data = await response.json();
-
+        
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
         alert(error);
@@ -51,7 +51,31 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
 
-  const handleSubmit =  () => {};
+  const handleSubmit =  async(e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,12 +4,11 @@ import { Card, FormField, Loader } from '../components';
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
-    return (
-      data.map((post) => <Card key={post._id} {...post} />)
-    );
+    return  data.map((post) => <Card key={post._id} {...post} />)
+    
   }
 
-  return (
+  return  (
     <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
   );
 };
@@ -20,7 +19,33 @@ const Home = () => {
 
   const [searchText, setSearchText] = useState('');
   
-  const [searchedResults, setSearchedResults] = useState(null);
+useEffect( () => {
+   const fetchPosts = async () =>  {
+     setLoading(true);
+
+     try {
+      const response = await fetch('http://localhost:8080/api/v1/post',{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if(response.ok){ 
+        const result = await response.json();
+
+        setAllPosts(result.data.reverse());
+      }
+     } catch (error) {
+       alert(error);
+     }
+     finally{
+      setLoading(false);
+     }
+    }
+     fetchPosts();
+} , []);
+
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -53,7 +78,7 @@ const Home = () => {
                 />
               ) : (
                 <RenderCards
-                  data={[]}
+                  data={allPosts}
                   title="No Posts Yet"
                 />
               )}
